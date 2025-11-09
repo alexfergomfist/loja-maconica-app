d36bd06e603d5b3fb1f220f851adfbb2d0f6e228
alexfergomfist <alexfergomfist@gmail.com>
2025-11-09 19:56:32 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='loja-cache-v1'; const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
+self.addEventListener('fetch',e=>{const req=e.request; e.respondWith(caches.match(req).then(c=>c||fetch(req).then(res=>{const copy=res.clone(); caches.open(CACHE).then(cache=>cache.put(req,copy)); return res;})).catch(()=>caches.match('./')))});
